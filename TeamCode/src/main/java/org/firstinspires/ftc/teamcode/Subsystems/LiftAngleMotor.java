a07b38bef552f40eee2bd538581e9c2d74325f49
biyo-u <biyouvbiama@gmail.com>
2024-11-01 19:43:26 -0600

    major additions to LiftAngleMotor.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/LiftAngleMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/LiftAngleMotor.java
@@ -5,7 +5,9 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import org.firstinspires.ftc.teamcode.Constants;
 public class LiftAngleMotor {
 
-    private final DcMotor liftanglemotor;
+    private DcMotor liftanglemotor;
+
+    public double location = liftanglemotor.getTargetPosition();
 
     public LiftAngleMotor(DcMotor liftanglemotor) {
         this.liftanglemotor = liftanglemotor;
@@ -14,6 +16,18 @@ public class LiftAngleMotor {
     public void LiftAngleMove(double speed, double location) {
         liftanglemotor.setTargetPosition(1);
         liftanglemotor.setPower(speed);
-        
+        liftanglemotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void LiftAngleFoward(double speed) {
+        LiftAngleMove(speed, liftanglemotor.getCurrentPosition());
+    }
+
+    public void LiftAngleBackward(double speed) {
+        LiftAngleMove(-speed, liftanglemotor.getCurrentPosition());
+    }
+
+    public void LiftAngleStop() {
+        liftanglemotor.setPower(0);
     }
 }
